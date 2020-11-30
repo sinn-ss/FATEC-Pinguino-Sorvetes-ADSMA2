ec2c8a019eecc6fac8e52309cd9cd3799da89108
Stephani Tiemi <stephanitiemi@gmail.com>
2020-11-30 10:38:00 -0300

    Oi

---
+++ Pinguino-Sorvetes/frm_pedido.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_pedido
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txt_id_cliente = New System.Windows.Forms.TextBox()
+        Me.txt_id_func = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ID do Cliente"
+        '
+        'txt_id_cliente
+        '
+        Me.txt_id_cliente.Location = New System.Drawing.Point(26, 29)
+        Me.txt_id_cliente.Name = "txt_id_cliente"
+        Me.txt_id_cliente.Size = New System.Drawing.Size(139, 20)
+        Me.txt_id_cliente.TabIndex = 1
+        '
+        'txt_id_func
+        '
+        Me.txt_id_func.Location = New System.Drawing.Point(26, 78)
+        Me.txt_id_func.Name = "txt_id_func"
+        Me.txt_id_func.Size = New System.Drawing.Size(139, 20)
+        Me.txt_id_func.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(23, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "ID do Funcionário"
+        '
+        'frm_pedido
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txt_id_func)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txt_id_cliente)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frm_pedido"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txt_id_cliente As TextBox
+    Friend WithEvents txt_id_func As TextBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Pinguino-Sorvetes/frm_pedido.vb
@@ -0,0 +1,3 @@
+﻿Public Class frm_pedido
+
+End Class
